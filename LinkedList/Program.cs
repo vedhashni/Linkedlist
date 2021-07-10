87ba439ce5f2de0b238f1a3824070e6a214145b1
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-10 22:20:53 +0530

    [Vedhashni] Updated the file welcome to linkedlist problem

---
+++ LinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList program");
+        }
+    }
+}
